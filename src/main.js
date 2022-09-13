c412e29611e16c904e47a8d967460171f4e9fa96
Edier Gama <ediersantiago7@gmail.com>
2022-09-12 22:37:30 -0500

    Trending movies function

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-console.log('hola' + APIKEY)
+async function getTrendingMoviesPreview() {
+   const res = await fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${APIKEY}`)
+   const data = await res.json()
+   const movies = data.results
+   console.log({data, movies});
+}
+
+getTrendingMoviesPreview()
