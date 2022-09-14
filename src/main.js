f40cae7c6a05827b9be9d3a8f0b6827a64287761
Edier Gama <ediersantiago7@gmail.com>
2022-09-13 21:26:20 -0500

    fetch a la API  moviedb para peliculas en tendencia

--- src/main.js
+++ src/main.js
@@ -2,7 +2,42 @@ async function getTrendingMoviesPreview() {
    const res = await fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${APIKEY}`)
    const data = await res.json()
    const movies = data.results
-   console.log({data, movies});
+   console.log(movies);
+
+   movies.forEach(movie => {
+      const trendingSection = document.querySelector('.trendingPreview-container')
+      const trendingMoviesContainer = document.querySelector('.trendingPreview-movieList')
+      const trendingMoviesDiv = document.createElement('div')
+      trendingMoviesDiv.classList.add('movie-container')
+      const trendingMoviesImg = document.createElement('img')
+      trendingMoviesImg.classList.add('movie-img')
+      trendingMoviesImg.setAttribute('alt', movie.title)
+      trendingMoviesImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300' + movie.poster_path)
+      trendingMoviesDiv.appendChild(trendingMoviesImg)
+      trendingMoviesContainer.appendChild(trendingMoviesDiv)
+      trendingSection.appendChild(trendingMoviesContainer)   
+   });
+}
+
+async function getTrendingMoviesPreview() {
+   const res = await fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${APIKEY}`)
+   const data = await res.json()
+   const movies = data.results
+   console.log(movies);
+
+   movies.forEach(movie => {
+      const trendingSection = document.querySelector('.trendingPreview-container')
+      const trendingMoviesContainer = document.querySelector('.trendingPreview-movieList')
+      const trendingMoviesDiv = document.createElement('div')
+      trendingMoviesDiv.classList.add('movie-container')
+      const trendingMoviesImg = document.createElement('img')
+      trendingMoviesImg.classList.add('movie-img')
+      trendingMoviesImg.setAttribute('alt', movie.title)
+      trendingMoviesImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300' + movie.poster_path)
+      trendingMoviesDiv.appendChild(trendingMoviesImg)
+      trendingMoviesContainer.appendChild(trendingMoviesDiv)
+      trendingSection.appendChild(trendingMoviesContainer)   
+   });
 }
 
 getTrendingMoviesPreview()
