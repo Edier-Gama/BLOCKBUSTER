28004e440a1c7a2c85e114a239c73b61caf9bd28
Edier Gama <ediersantiago7@gmail.com>
2022-09-16 20:48:35 -0500

    pruebas con API KEY explicita

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const APIKEY = '21f822852ba5dced7872096c73b52c4a'
+
 
 function createMovies(movies, container) {
 
@@ -43,7 +43,7 @@ function createCategories(categories, container) {
 }
 
 async function getTrendingMoviesPreview() {
-   const res = await fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${APIKEY}`)
+   const res = await fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=21f822852ba5dced7872096c73b52c4a`)
    data = await res.json()
    const movies = data.results
 
@@ -52,7 +52,7 @@ async function getTrendingMoviesPreview() {
 }
 
 async function getCategoriesPreview() {
-   const res = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${APIKEY}`)
+   const res = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=21f822852ba5dced7872096c73b52c4a`)
    const data = await res.json()
    const categories = data.genres
 
@@ -61,7 +61,7 @@ async function getCategoriesPreview() {
 }
 
 async function getMovieByCategorie(id) {
-   const res = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${APIKEY}&with_genres=${id}`)
+   const res = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=21f822852ba5dced7872096c73b52c4a&with_genres=${id}`)
    const data = await res.json()
    const movies = data.results
 
@@ -69,7 +69,7 @@ async function getMovieByCategorie(id) {
 }
 
 async function searchMovie(query){
-   const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&query=${query}`)
+   const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=21f822852ba5dced7872096c73b52c4a&query=${query}`)
    const data = await res.json()
    const movies = data.results
 
@@ -83,7 +83,7 @@ async function getTrendingMovies() {
 }
 
 async function getMovieById(id) {
-   const res = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${APIKEY}`)
+   const res = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=21f822852ba5dced7872096c73b52c4a`)
    const data = await res.json()
 
    const movieImgUrl = 'https://image.tmdb.org/t/p/w500' + data.poster_path
@@ -107,7 +107,7 @@ async function getMovieById(id) {
 }
 
 async function getRelatedMoviesById(id) {
-   const res = await fetch(`https://api.themoviedb.org/3/movie/${id}/similar?api_key=${APIKEY}`)
+   const res = await fetch(`https://api.themoviedb.org/3/movie/${id}/similar?api_key=21f822852ba5dced7872096c73b52c4a`)
    const data = await res.json()
 
    movies = data.results
